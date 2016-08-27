1d06948fb1a2835fad9848f11f05399485163877
Doug Cichon <djcichon@gmail.com>
2016-08-27 02:05:45 +0000

    Finish log in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = "Welcome to the Sample App, #{@user.name}"
+      log_in @user
       redirect_to @user
     else
       render 'new'
