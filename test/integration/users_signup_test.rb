41e13976e6b22bf9a72b5c92073f8bc359d948a4
Doug Cichon <djcichon@gmail.com>
2016-09-01 02:05:00 +0000

    Add account activations

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invalid signup information" do
     get signup_path
     
@@ -17,21 +22,40 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.field_with_errors", 8 # Two for each field (label and field)
   end
   
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "Example User",
-                                    email: "user@example.com", 
-                                    password: "password", 
-                                    password_confirmation: "password" }
+      post users_path, user: { name: "Example User",
+                               email: "user@example.com", 
+                               password: "password", 
+                               password_confirmation: "password" }
     end
     
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    
+    assert_not_nil user
+    assert_not user.activated?
+    
+    # Try to login before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    
+    # Invalid activation token
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+    
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    
     assert_template 'users/show' 
-    
-    assert_select "div#error_explanation", false
-    assert_select "div.field_with_errors", false
-    
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
