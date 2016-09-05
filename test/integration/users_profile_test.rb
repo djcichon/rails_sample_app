3d5af1e16eac4043440f5cf1682c979210ff3c12
Doug Cichon <djcichon@gmail.com>
2016-09-05 15:34:19 +0000

    Completed exercise for chapter 11

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -17,6 +17,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     
+    assert_select 'strong#following', text: /#{@user.following.count}/ 
+    assert_select 'strong#followers', text: /#{@user.followers.count}/ 
+    
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
